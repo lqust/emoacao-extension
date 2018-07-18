a7358ebcb5add6c9ad615bbb72370dc18fb5b6c0
Lourenço P. Soares <lousbr@gmail.com>
2018-07-18 14:30:09 -0300

    Version that works logging to console stable.  Cleaned code a bit.

--- popup/select_state_of_mind.js
+++ popup/select_state_of_mind.js
@@ -1,56 +1,27 @@
-/**
- * CSS to hide everything on the page,
- * except for elements that have the "emoacao-image" class.
- */
-const hidePage = `body > :not(.emoacao-image) {
-                    display: none;
-                  }`;
-
-/**
- * Listen for clicks on the buttons, and send the appropriate message to
- * the content script in the page.
- */
 function listenForClicks() {
+
   document.addEventListener("click", (e) => {
 
     function registerSoM(stateOfMind) {
 
       var currentdate = new Date(); 
-      var datetime = currentdate.getFullYear() + "-"
-                   + (currentdate.getMonth()+1)  + "-" 
-                   + currentdate.getDate() + " "  
-                   + currentdate.getHours() + ":"  
-                   + currentdate.getMinutes() + ":" 
-                   + currentdate.getSeconds();
+      var datetime = currentdate.getFullYear() + "-" + (currentdate.getMonth()+1)  + "-"  + currentdate.getDate() + " "  
+                   + currentdate.getHours() + ":" + currentdate.getMinutes() + ":" + currentdate.getSeconds();
 
       switch (stateOfMind) {
-        case "Sem foco & Feliz":
-          return datetime + ",0,1";
-        case "Com foco & Triste":
-          return datetime + ",1,0";
-        case "Sem foco & Triste":
-          return datetime + ",0,0";
-        case "Com foco & Feliz":
-          return datetime + ",1,1";
+        case "Sem foco & Feliz":  return datetime + ",0,1";
+        case "Com foco & Triste": return datetime + ",1,0";
+        case "Sem foco & Triste": return datetime + ",0,0";
+        case "Com foco & Feliz":  return datetime + ",1,1";
+
       }
     }
 
     function emoacao(tabs) {
-      browser.tabs.insertCSS({code: hidePage}).then(() => {
         let stateOfMindRow = registerSoM(e.target.textContent);
-        browser.tabs.sendMessage(tabs[0].id, {
-          command: "emoacao",
-          stateOfMind: stateOfMindRow
-        });
-
         console.log(stateOfMindRow);
-
-        });
       }
 
-    /**
-     * Just log the error to the console.
-     */
     function reportError(error) {
       console.error(`Could not register Emotion: ${error}`);
     }
@@ -60,28 +31,8 @@ function listenForClicks() {
     .catch(reportError);
    
   });
+  
 }
 
-/**
- * There was an error executing the script.
- * Display the popup's error message, and hide the normal UI.
- */
-function reportExecuteScriptError(error) {
-  document.querySelector("#popup-content").classList.add("hidden");
-  document.querySelector("#error-content").classList.remove("hidden");
-  console.error(`Failed to execute emoacao content script: ${error.message}`);
-}
+listenForClicks();
 
-console.log("asaasdsajkfsdlkjfsdlkjfsdfasfjkhslfhsafklshfkslj");
-
-/**
- * When the popup loads, inject a content script into the active tab,
- * and add a click handler.
- * If we couldn't inject the script, handle the error.
-
-
-browser.tabs.executeScript({file: "/content_scripts/emoacao.js"})
-.then(listenForClicks)
-.catch(reportExecuteScriptError);
-
-*/
