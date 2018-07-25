5b56f504fd7b82658f1993af867aac58677a2da2
Lourenço P. Soares <lousbr@gmail.com>
2018-07-25 14:35:48 -0300

    Deveria estar funcionando com o listener

--- popup/select_state_of_mind.js
+++ popup/select_state_of_mind.js
@@ -1,52 +1,46 @@
+const postURL = "https://script.google.com/macros/d/1J2cVAQohwv58U94o3yIVYu4xg9ScrORK--n5oo9sqvY_WAYfkHKlLNd4/exec"
 
-const gsURL = "https://script.google.com/macros/d/1J2cVAQohwv58U94o3yIVYu4xg9ScrORK--n5oo9sqvY_WAYfkHKlLNd4/exec"
+function storeData(postBody) {  
 
-function postToSpreadsheet(stateOfMindData) {
+  console.log(postURL);
+  console.log(postBody);
 
   var xhr = new XMLHttpRequest();
-  xhr.open("POST", gsURL, true);
-  xhr.setRequestHeader('Content-Type', 'text/plain');
-  xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
-  xhr.send(stateOfMindData);
+  xhr.open('POST', postURL);
+  // xhr.withCredentials = true;
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  xhr.onreadystatechange = function() {
+      console.log( xhr.status, xhr.statusText )
+      console.log(xhr.responseText);
+      return;
+  };
+  // url encode form data for sending as post data
+  var encoded = Object.keys(data).map(function(k) {
+      return encodeURIComponent(k) + "=" + encodeURIComponent(data[k])
+  }).join('&')
+  xhr.send(encoded);
+  
+}
+
+function registerSoM(stateOfMind) {
+  var currentdate = new Date(); 
+  var datetime = currentdate.getFullYear() + "-" + (currentdate.getMonth()+1)  + "-"  + currentdate.getDate() + " "  
+                + currentdate.getHours() + ":" + currentdate.getMinutes() + ":" + currentdate.getSeconds();
+
+  switch (stateOfMind) {
+    case "0,1":  return "{ timestamp:" + datetime + ", foco:0, feliz:1 }";
+    case "1,0": return "{ timestamp:" + datetime + ", foco:1, feliz:0 }";
+    case "0,0": return "{ timestamp:" + datetime + ", foco:0, feliz:0 }";
+    case "1,1":  return "{ timestamp:" + datetime + ", foco:1, feliz:1 }";
+  }
 
 }
 
-
 function listenForClicks() {
-
   document.addEventListener("click", (e) => {
-
-    function registerSoM(stateOfMind) {
-
-      var currentdate = new Date(); 
-      var datetime = currentdate.getFullYear() + "-" + (currentdate.getMonth()+1)  + "-"  + currentdate.getDate() + " "  
-                   + currentdate.getHours() + ":" + currentdate.getMinutes() + ":" + currentdate.getSeconds();
-
-      switch (stateOfMind) {
-        case "Sem foco & Feliz":  return "{ timestamp:" + datetime + ", foco:0, feliz:1 }";
-        case "Com foco & Triste": return "{ timestamp:" + datetime + ", foco:1, feliz:0 }";
-        case "Sem foco & Triste": return "{ timestamp:" + datetime + ", foco:0, feliz:0 }";
-        case "Com foco & Feliz":  return "{ timestamp:" + datetime + ", foco:1, feliz:1 }";
-
-      }
-    }
-
-    function emoacao(tabs) {
-        let stateOfMindRow = registerSoM(e.target.textContent);
-        postToSpreadsheet(stateOfMindRow);
-      }
-
-    function reportError(error) {
-      console.error(`Could not register Emotion: ${error}`);
-    }
-
-   browser.tabs.query({active: true, currentWindow: true})
-    .then(emoacao)
-    .catch(reportError);
-   
+    console.log("oi");
+    storeData(registerSoM(e.target.textContent));
   });
-
 }
 
 listenForClicks();
-
