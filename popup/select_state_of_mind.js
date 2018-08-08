b4bf514c1daa33732df7a54c630e85ca6c92283b
Lourenço P. Soares <lousbr@gmail.com>
2018-08-08 16:34:32 -0300

    Revert "Testando headers de segurança"
    This reverts commit 4bd853b042039e0d80a96413e1b5cd5290cf2eea.

--- popup/select_state_of_mind.js
+++ popup/select_state_of_mind.js
@@ -1,62 +1,41 @@
+const postURL = "https://script.google.com/a/thoughtworks.com/macros/s/AKfycbxkA-kYvVfCI_k_q0Qn96CmQ2y0MaL2NrnLgAikInW5G_rt15s/exec"
 
-  function handleFormSubmit(event) {  // handles form submit without any jquery
-    event.preventDefault();           // we are submitting via xhr below
+window.googleDocCallback = function () { return true; };
 
-    let stateOfMindRow = document.getElementById("som");
-    var data = postToSpreadsheet(stateOfMindRow);   
+function storeData(postBody) {  
 
-    disableAllButtons(event.target);
-    var url = event.target.action;  //
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', url);
-    // xhr.withCredentials = true;
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.onreadystatechange = function() {
-        console.log( xhr.status, xhr.statusText )
-        console.log(xhr.responseText);
-        return;
-    };
-    // url encode form data for sending as post data
-    var encoded = Object.keys(data).map(function(k) {
-        return encodeURIComponent(k) + "=" + encodeURIComponent(data[k])
-    }).join('&')
-    xhr.send(encoded);
-    
-  }
-  function loaded() {
-    console.log("Contact form submission handler loaded successfully.");
-    // bind to the submit event of our form
-    var form = document.getElementById("gform");
-    form.addEventListener("submit", handleFormSubmit, false);
-  };
-  document.addEventListener("DOMContentLoaded", loaded, false);
+  console.log(postURL);
+  console.log(postBody);
 
-  function disableAllButtons(form) {
-    var buttons = form.querySelectorAll("button");
-    for (var i = 0; i < buttons.length; i++) {
-      buttons[i].disabled = true;
-    }
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', postURL);
+  // xhr.withCredentials = true;
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  xhr.send(postBody);
+  
 }
 
 function registerSoM(stateOfMind) {
-
   var currentdate = new Date(); 
   var datetime = currentdate.getFullYear() + "-" + (currentdate.getMonth()+1)  + "-"  + currentdate.getDate() + " "  
                 + currentdate.getHours() + ":" + currentdate.getMinutes() + ":" + currentdate.getSeconds();
 
   switch (stateOfMind) {
-    case "0,1":  return "{ timestamp:" + datetime + ", foco:0, feliz:1 }";
-    case "1,0": return "{ timestamp:" + datetime + ", foco:1, feliz:0 }";
-    case "0,0": return "{ timestamp:" + datetime + ", foco:0, feliz:0 }";
-    case "1,1":  return "{ timestamp:" + datetime + ", foco:1, feliz:1 }";
-
+    case "Sem foco & Feliz":  return "{ timestamp:" + datetime + ", foco:0, feliz:1 }";
+    case "Com foco & Triste": return "{ timestamp:" + datetime + ", foco:1, feliz:0 }";
+    case "Sem foco & Triste": return "{ timestamp:" + datetime + ", foco:0, feliz:0 }";
+    case "Com foco & Feliz":  return "{ timestamp:" + datetime + ", foco:1, feliz:1 }";
   }
+
 }
 
-document.getElementsByTagName("td").onclick({
-  
-//  var inputSoM = document.getElementById('som');
-//  inputSoM.value = "";
+function listenForClicks() {
+  document.addEventListener("click", (e) => {
+    let stateOfMind = registerSoM(e.target.textContent);
+    storeData(stateOfMind);
+  });
+}
 
-})
+console.log("rodando...");
 
+listenForClicks();
