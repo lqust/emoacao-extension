cf813388ca71eab36e67b2e881acdb78d411cc80
Lourenço P. Soares <lousbr@gmail.com>
2018-08-16 17:51:14 -0300

    Formatando corretamente o parâmetro do POST

--- popup/select_state_of_mind.js
+++ popup/select_state_of_mind.js
@@ -1,4 +1,5 @@
-const postURL = "https://script.google.com/a/thoughtworks.com/macros/s/AKfycbxkA-kYvVfCI_k_q0Qn96CmQ2y0MaL2NrnLgAikInW5G_rt15s/exec"
+
+const postURL = "https://script.google.com/macros/s/AKfycbxkA-kYvVfCI_k_q0Qn96CmQ2y0MaL2NrnLgAikInW5G_rt15s/exec";
 
 window.googleDocCallback = function () { return true; };
 
@@ -9,7 +10,6 @@ function storeData(postBody) {
 
   var xhr = new XMLHttpRequest();
   xhr.open('POST', postURL);
-  // xhr.withCredentials = true;
   xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 
   xhr.onreadystatechange = function() {
@@ -17,30 +17,25 @@ function storeData(postBody) {
       
       console.log(xhr.responseText);
 
-      var response = JSON.parse(xhr.responseText);
-        if (xhr.status === 200 && response.status === 'OK') {
-           console.log('successful');
-        } else {
-           console.log('failed');
-        }
     }
   }
 
-  xhr.send(postBody);
+  xhr.send(JSON.stringify(postBody));
   
 }
 
 function registerSoM(stateOfMind) {
-  var currentdate = new Date(); 
-  var datetime = currentdate.getFullYear() + "-" + (currentdate.getMonth()+1)  + "-"  + currentdate.getDate() + " "  
-                + currentdate.getHours() + ":" + currentdate.getMinutes() + ":" + currentdate.getSeconds();
+
+  var outputSoM;
 
   switch (stateOfMind) {
-    case "Sem foco & Feliz":  return "{ timestamp:" + datetime + ", foco:0, feliz:1 }";
-    case "Com foco & Triste": return "{ timestamp:" + datetime + ", foco:1, feliz:0 }";
-    case "Sem foco & Triste": return "{ timestamp:" + datetime + ", foco:0, feliz:0 }";
-    case "Com foco & Feliz":  return "{ timestamp:" + datetime + ", foco:1, feliz:1 }";
+    case "Sem foco & Feliz" : outputSoM = { "foco":"0", "feliz":"1" };
+    case "Com foco & Triste": outputSoM = { "foco":"1", "feliz":"0" }; 
+    case "Sem foco & Triste": outputSoM = { "foco":"0", "feliz":"0" }; 
+    case "Com foco & Feliz" : outputSoM = { "foco":"1", "feliz":"1" };
   }
+
+  return outputSoM;
 
 }
 
