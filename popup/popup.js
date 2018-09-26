138a4538544ba8bb8b9ad9694d3381cf688a899b
Lourenço P. Soares <lousbr@gmail.com>
2018-09-26 16:08:01 -0300

    Stylez

--- popup/popup.js
+++ popup/popup.js
@@ -13,14 +13,6 @@ function identifyBrowser() {
   }
 } // identifyBrowser
 
-function registerSoM(stateOfMind) {
-  switch (stateOfMind) {
-    case "com-foco-triste": return { "foco":"1", "feliz":"0", "time": teamName }; 
-    case "com-foco-feliz" : return { "foco":"1", "feliz":"1", "time": teamName };
-    case "sem-foco-triste": return { "foco":"0", "feliz":"0", "time": teamName }; 
-    case "sem-foco-feliz" : return { "foco":"0", "feliz":"1", "time": teamName };
-  }
-} // registerSoM
 
 function listenForClicks() {
 
@@ -29,13 +21,29 @@ function listenForClicks() {
     switch (clickTarget.id) {
       case "save-team": 
         document.cookie="emoacao-team=" + document.getElementById("team-name").value; // set team name
+        window.close();
         break;
       case "reset-team":
         document.cookie="emoacao-team=";
+        window.close();
         break;
-      default: port.postMessage(registerSoM(clickTarget.id));
+      case "com-foco-triste":
+        port.postMessage({ "foco":"1", "feliz":"0", "time": teamName }); 
+        window.setTimeout(function() {window.close()},100);
+        break;
+      case "com-foco-feliz" :
+        port.postMessage({ "foco":"1", "feliz":"1", "time": teamName });
+        window.setTimeout(function() {window.close()},100);
+        break;
+      case "sem-foco-triste":
+        port.postMessage({ "foco":"0", "feliz":"0", "time": teamName }); 
+        window.setTimeout(function() {window.close()},100);
+        break;
+      case "sem-foco-feliz" :
+        port.postMessage({ "foco":"0", "feliz":"1", "time": teamName });
+        window.setTimeout(function() {window.close()},100);
+        break;
     }
-    window.close();
 
   });
 } // listenForClicks
